4dee51aca100f29c2686ff42327102e918fa88c1
yourGitHubUserName <yourGitHub@email.com>
2019-04-28 18:06:02 -0400

    added navbar and modulated html files

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout_request, name='logout'),
+    path('login/', views.login_request, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 # Create your views here.
 
 def homepage(request):
     return render(request, 'main/homepage.html')
+
+def signup(request):
+    if request.method == "POST":  
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
+            login(request, user)
+            return redirect('main:homepage')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg}:{form.error_messages[msg]}")
+
+    form = UserCreationForm
+    return render(request, 
+                  'main/signup.html',
+                  context={'form': form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'You are now logged in as {username}')
+                return redirect('main:homepage')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else: 
+            messages.error(request, 'Invalid username or password. Likely missing a field.')
+   
+    form = AuthenticationForm
+    return render(request, 
+                  'main/login.html',
+                  {'form': form})
+
+
+def logout_request(request):
+    logout(reqeust)
+    messages.info(request, 'Logged out successfully')
+    return redirect('main:homepage')
