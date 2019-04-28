6dd72652919b72a96d845f970a01a27af2fdcb91
yourGitHubUserName <yourGitHub@email.com>
2019-04-28 17:26:33 -0400

    initial user, lab, and todo model seems to work

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.widgets import TinyMCE
 
 # Create your models here.
+
+class Lab(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    start_date = models.DateField()
+    due_date = models.DateField()
+    users = models.ManyToManyField(User)
+
+    class Meta: 
+        verbose_name_plural = 'Labs'
+
+    def __str__(self):
+        return self.title
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    completeBool = models.BooleanField()
+    lab = models.ForeignKey(Lab, default=1, verbose_name=("Lab"), on_delete=models.SET_DEFAULT)
+    
+    class Meta: 
+        verbose_name_plural = 'Todos'
+
+    def __str__(self):
+        return self.title
--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('main.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage')
 ]
