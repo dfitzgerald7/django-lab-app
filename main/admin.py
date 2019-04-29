e3189933fa98b39d7fc94cd1079bc73127011e31
yourGitHubUserName <yourGitHub@email.com>
2019-04-29 10:58:18 -0400

    can create labs and todos in admin. cannot add user to lab or todos with lab

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Lab, Todo
 # Register your models here.
+
+class LabAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Lab)
+admin.site.register(Todo)
